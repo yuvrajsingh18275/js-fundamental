c17d17feff7ea78148b2a08ab4319edd7e3777c9
yuvrajsingh18275 <yuvrajjsingh02@gmail.com>
2025-07-24 12:13:19 +0530

    we learn function

--- function.js
+++ function.js
@@ -1,3 +1,64 @@
-let user="yuvraj"
-console.log(user.substring(4,2));
-console.log(user.slice(4,2));
+// let user="yuvraj"
+// console.log(user.substring(4,2));
+// console.log(user.slice(4,2));
+
+// let user = "ganesh";
+// for (const element of user)
+// {
+//     console.log(element);
+// }
+
+// const a=[1,2,3,4,5]
+// console.log(typeof(a));
+
+// const user=[10,20,30,40,50];
+// const user1=[1,2,3];
+// console.log(...user,...user1);
+
+// let user="yuvraj";
+// const user1=[10,20,30,40,50];
+// console.log(Array.isArray(user));
+// console.log(Array.isArray(user1));
+
+// let num=10;
+// let num1=20;
+// let num2=30;
+// console.log(Array.of(num,num1,num2));
+
+// const user=[10,20,30,40,50];
+// console.log(user.slice(1,3));
+// console.log(user.splice(2,4));
+
+// const user=[10,20,30,40,50];
+// console.log("before",user);
+// const newuser=user.splice(0,3);
+// console.log("newuser",newuser);
+// console.log("after",user);
+
+// const user=[10,20,30,40,50];
+// for (const value of user){
+//     console.log(value);
+// }
+
+// const user=[10,20,30,40,50];
+//  for (const element in user){
+//         console.log(user);
+//  }
+
+// const userID1=[10,20,30,40,50];
+// userID1.forEach(function addFive(val){
+//     console.log(val+5);
+
+// })
+// const user=[10,20,30,40,50];
+// user.forEach((val)=>{
+//     console.log(val+5);
+// })
+
+
+const addNum=(num1,num2)=>{
+    console.log(num1);
+    console.log(num2);
+    console.log(num1+num2);
+}
+addNum(18,8);
